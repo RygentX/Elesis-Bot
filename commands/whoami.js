1526ad19d8987ec91b4d94ef37f5799556d53220
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 16:01:18 +0700

    Create whoami.js

---
+++ commands/whoami.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	let uicon = message.author.displayAvatarURL;
+	let userembed = new Discord.RichEmbed()
+	.setAuthor(message.author.tag)
+	.setThumbnail(uicon)
+	.addField("ID", message.author.id, true)
+	.addField("Tanggal bergabung Discord", message.author.createdAt, true)
+	.addField("Tanggal bergabung Server", message.member.joinedAt, true)
+	.addField("Nama panggilan", message.author.nickname || 'None', true)
+	
+	message.channel.send(userembed);
+}
+
+module.exports.help = {
+	name: "whoami",
+	description: "Kirim informasi tentang Pengguna.",
+	usage: "/whoami"
+}
