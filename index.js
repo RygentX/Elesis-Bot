965a3c7671d6c9ba92b9e5dc7cd0a2414c76f719
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 10:44:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 const fs = require("fs");
 const config = require("./config.json");
-const bot = new discord.Client({disableEveryone: true});
+const bot = new discord.Client({disableEveryone: false});
 
 // When bot ready
 bot.on("ready", async () => {
