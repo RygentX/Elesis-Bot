93ed7fe25dfd662ffd59ca9b85f1591c7fb24e4a
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 15:45:12 +0700

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
 
-    let reason = args.join(' ') ? args.join(' ') : 'I am currently afk, I will reply as soon possible.';
+    let reason = args.join(' ') ? args.join(' ') : 'Saya saat ini sedang afk, saya akan membalas sesegera mungkin.';
     let afklist = bot.afk.get(message.author.id);
 
     if (!afklist) {
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
         };
 
         bot.afk.set(message.author.id, construct);
-        return message.reply(`you have been set to afk for reason: ${reason}`).then(msg => msg.delete(5000));
+        return message.reply(`Anda telah diatur ke afk karena alasan: ${reason}`).then(msg => msg.delete(5000));
     }
 
 };
