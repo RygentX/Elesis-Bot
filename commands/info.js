32c48eca51bf2f973bc37960a2d10410e5dc48a3
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 16:10:07 +0700

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	let bicon = bot.user.displayAvatarURL;
+	let botembed = new Discord.RichEmbed()
+	.setAuthor(bot.user.tag)
+	.setThumbnail(bicon)
+	.addField("ID", bot.user.id, true)
+	.addField("Tanggal bergabung Discord", bot.user.createdAt, true)
+	.addField("Nama panggilan", bot.user.nickname || 'Tidak ada', true)
+	
+	message.channel.send(botembed);
+}
+
+module.exports.help = {
+	name: "info",
+	description: "Kirim informasi tentang bot.",
+	usage: "/info"
+}
