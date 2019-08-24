d8d11783e47ca8cbbc45ac318c707eb0f72bd628
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 15:52:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ bot.on("message", async message => {
     
     if (message.content.includes(message.mentions.users.first())) {
     let mentioned = bot.afk.get(message.mentions.users.first().id);
-    if (mentioned) message.channel.send(`**${mentioned.usertag}** is currently afk. Reason: ${mentioned.reason}`);
+    if (mentioned) message.channel.send(`**${mentioned.usertag}** sedang AFK!\nKarena: ${mentioned.reason}`);
     }
     let afkcheck = bot.afk.get(message.author.id);
-    if (afkcheck) return [bot.afk.delete(message.author.id), message.reply(`you have been removed from the afk list!`).then(msg => msg.delete(5000))];
+    if (afkcheck) return [bot.afk.delete(message.author.id), message.reply(`Anda telah dihapus dari daftar afk!`).then(msg => msg.delete(5000))];
     
     if (!command.startsWith(prefix)) return;
     
