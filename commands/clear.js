39779066733db80b467e2e8645aee53a42318065
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 10:24:03 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	
-	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-	if(!args[0]) return message.channel.send("no");
+	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Siapa ini yang bukan admin memberikan perintah kepada saya?");
+	if(!args[0]) return message.channel.send("Berikan jumlah pesan yang ingin dihapus");
 	message.channel.bulkDelete(args[0]).then(() => {
-		message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+		message.channel.send(`${args[0]} pesan telah dihapus.`).then(msg => msg.delete(2000));
 	});
 
 }
 
 module.exports.help = {
 	name: "clear",
-	description: "Clears messages from current channel.",
+	description: "Menghapus pesan dari saluran saat ini.",
 	usage: "/clear [number]"
 }
