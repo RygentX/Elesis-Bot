0324bb7ca1602ad6b1634451458cfd83ce07e3dc
rygent <54457437+rygent@users.noreply.github.com>
2019-08-24 15:14:49 +0700

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,9 +4,9 @@ const { RichEmbed } = require("discord.js");
 module.exports.run = (bot, message, args) => {
 
     const embed = new RichEmbed()
-    .setFooter(`Terakhir dimulai` bot.readyAt)
+    .setFooter('Terakhir dimulai' bot.readyAt)
     .setDescription(ms(bot.uptime))
-    .setAuthor(`Uptime`);
+    .setAuthor('Uptime');
     message.channel.send(embed);
 }
 
