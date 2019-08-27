5e0e5ff56bd4f88509139edc1758adc2b4875f75
rygent <54457437+rygent@users.noreply.github.com>
2019-08-28 05:31:01 +0700

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,6 +4,7 @@ const { RichEmbed } = require("discord.js");
 module.exports.run = (bot, message, args) => {
 
     const embed = new RichEmbed()
+    .setColor("#FFE80D")
     .setFooter(bot.readyAt)
     .setDescription(ms(bot.uptime))
     .setAuthor(bot.user.tag, bot.user.displayAvatarURL);
