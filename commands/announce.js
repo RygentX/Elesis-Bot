20f887fa70200876a015edc14218b288a77eaf18
rygent <54457437+rygent@users.noreply.github.com>
2019-08-28 04:48:22 +0700

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	if(!message.member.hasPermission("ADMINISTRATOR")) return;
 	const embed = new Discord.RichEmbed()
-	.setColor("#FCE300")
+	.setColor("#FFE80D")
 	.setDescription(args.join(" "));
 	
 	message.delete().catch();
